d29abc6d7f35a998c55977394a2260cc0026b466
林世鹏 <75055363@qq.com>
2023-05-02 22:58:07 +0800

    ignore

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    title: "HNU-RM共享平台",
+    description: "该网站用于海南大学RoboMaster战队作为共享知识库",
+    markdown: {
+        lineNumbers: true,
+    },
+    themeConfig: {
+        sidebar: [
+            {
+                title: 'HNU-RM共享平台',
+                children: ['/视觉组周报/视觉组-林世鹏-第1周'],
+                initialOpenGroupIndex: 1,
+            }
+        ]
+    },
+};
---
+++ docs/README.md
@@ -0,0 +1,15 @@
+---
+home: true
+heroText: 一个共享知识库 heroText
+tagline: 共享知识库 tagline
+actionText: 查看 actionText
+actionLink: /视觉组周报/视觉组-林世鹏-第1周
+features:
+- title: 视觉组
+  details: 视觉组共享知识库
+- title: 电控组
+  details: 电控组共享知识库
+- title: 机械组
+  details: 机械组共享知识库
+footer: 共享平台 | footer
+---

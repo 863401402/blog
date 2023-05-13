84141e57eaa92778889cce251b1faabe4a46b24c
林世鹏 <75055363@qq.com>
2023-05-13 23:32:43 +0800

    ignore

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    title: "HNU-RM共享平台",
+    title: "海南大学RM共享平台",
     description: "该网站用于海南大学RoboMaster战队作为共享知识库",
     base: "/blog/",
     markdown: {
@@ -8,7 +8,7 @@ module.exports = {
     themeConfig: {
         sidebar: [
             {
-                title: 'HNU-RM共享平台',
+                title: '海南大学RM共享平台',
                 children: ['/视觉组周报/视觉组-林世鹏-第1周'],
                 initialOpenGroupIndex: 1,
             }
